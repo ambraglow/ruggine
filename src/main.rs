e124569470666fd1d85e66ebc2250b85d71f79a3
ambraglow <ambra@ambraglow.org>
2025-11-27 16:11:43 +0100

    define globals, change code to work with new implementation

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,34 @@
 mod fft;
 mod graph;
 
-use crate::{fft::complex::Sine, graph::Plot};
+use crate::{
+    fft::{
+        complex::{RandComplex, Sine},
+        dft::{Dft, FourierTransform},
+    },
+    graph::Plot,
+};
 use anyhow::{Ok, Result};
-use num_complex::Complex32;
+
+const SAMPLE_RATE: u32 = 600;
+const NR_SAMPLES: usize = 1000;
 
 fn main() -> Result<(), anyhow::Error> {
-    // let random_vectors = RandComplex::random_vec(300, 1.0).unwrap();
-    // let re: Vec<Complex64> = random_vectors
-    //     .iter()
-    //     .map(|item: &Complex64| Complex64::new(item.re().cos(), item.im().sin()))
-    //     .collect();
-    // //let re: Vec<f64> = re.iter().map(|x| x.re + x.im).collect();
-    // println!("{:?}", re);
+    let mut dft = FourierTransform::new();
 
-    // let mut sigout: Vec<Complex64> = Vec::new();
-    // let mut sum = Complex64::new(0.0, 0.0);
-    // for item in &re {
-    //     sum += Complex64::new(item.re() + item.im(), item.re());
-    //     sigout.push(sum);
-    //     //println!("{:?}", sigout);
-    // }
+    let sine1 = Sine::new(200f32, SAMPLE_RATE);
+    // let sine2 = Sine::new(100f32, SAMPLE_RATE);
+    let sine1: Vec<f32> = sine1.take(NR_SAMPLES).collect();
+    // let sine2: Vec<f32> = sine2.take(NR_SAMPLES).collect();
 
-    let sample_rate = 44000;
-    let frequency = 1000f32;
+    // dft.signal.append(&mut sine1);
 
-    let sine2 = Sine::new(frequency, sample_rate);
-    let mut sine2: Vec<f32> = sine2.take(10).collect();
-    println!("{sine2:?}");
+    dft.signal = RandComplex::random_vec(NR_SAMPLES, 0.1).unwrap();
+    // dft.signal = sine1.iter().zip(sine1).map(|(a, b)| a + b).collect();
+    dft.signal = sine1.iter().zip(dft.signal).map(|(a, b)| a + b).collect();
 
-    // sine2 = sine2.iter().map(|a| a * 10 as f32).collect();
-    // println!("{:?}", sine2.len());
-
-    sine2.clone().plot(Some("sine".to_string()))?;
-    let dft: Vec<Complex32> = fft::dft::dft(&mut sine2, Some(false)).unwrap();
-    println!("{dft:?} length: {:?}", dft.len());
+    dft.signal.clone().plot(Some("signal".to_string()))?;
+    dft.dft();
 
     dft.plot(Some("dft".to_string()))?;
 
