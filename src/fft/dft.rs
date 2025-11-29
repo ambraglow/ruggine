999682f087aab74aa00c4f668275f9f4f58a5377
ambraglow <ambra@ambraglow.org>
2025-11-29 21:23:52 +0100

    writing tests!

--- src/fft/dft.rs
+++ src/fft/dft.rs
@@ -92,6 +92,64 @@ impl Dft for FourierTransform {
     }
 }
 
+#[allow(dead_code)]
+fn split(input: &mut FourierTransform) -> Result<(Vec<f32>, Vec<f32>)> {
+    let (even_indices, odd_indices): (Vec<_>, Vec<_>) = input
+        .signal
+        .iter()
+        .enumerate()
+        .partition(|(i, _)| i % 2 == 0);
+
+    let even: Vec<f32> = even_indices.into_iter().map(|(_, val)| *val).collect();
+    let odd: Vec<f32> = odd_indices.into_iter().map(|(_, val)| *val).collect();
+
+    Ok((even, odd))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Sine;
+
+    const SAMPLE_RATE: u32 = 500;
+    const NR_SAMPLES: usize = 1000;
+
+    fn signal_helper() -> Result<FourierTransform> {
+        let mut dft = FourierTransform::new();
+        let sine1: Vec<f32> = Sine::new(200f32, SAMPLE_RATE).take(NR_SAMPLES).collect();
+
+        dft.signal = sine1.iter().zip(dft.signal).map(|(a, b)| a + b).collect();
+
+        Ok(dft)
+    }
+
+    #[test]
+    fn running_dft() {
+        let mut dft = signal_helper().unwrap();
+        let signal_len: usize = dft.signal.len();
+
+        dft.dft();
+        assert_eq!(signal_len, dft.bins.len());
+    }
+
+    #[test]
+    fn splitting() {
+        let mut dft = signal_helper().unwrap();
+        let signal_len: usize = dft.signal.len();
+
+        let mut even: Vec<_> = vec![f32::default(); signal_len];
+        let mut odd: Vec<_> = vec![f32::default(); signal_len];
+
+        (even, odd) = split(&mut dft).unwrap();
+
+        let size_of_odd: usize = odd.len();
+        let size_of_even: usize = even.len();
+        let tot_size: usize = size_of_even + size_of_odd;
+
+        assert_eq!(signal_len, tot_size);
+    }
+}
+
 // This is basically what happens in the iterator but as for loops, code kept for reference
 // for frequency_bin in 0..signal {
 //     let mut sum: Complex32 = Complex32::default();
